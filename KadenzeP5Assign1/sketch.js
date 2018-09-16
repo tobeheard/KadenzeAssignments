b56ce865405ec3d560307130b46d06e8b5b75ba1
Working Engine <workingengine@sharyns-MacBook-Pro.local>
2018-09-16 15:56:52 +1200

    A place for p5 Assignments Introduction to Programming for the Visual Arts with p5.js

---
+++ KadenzeP5Assign1/sketch.js
@@ -0,0 +1,22 @@
+// For this assignment:
+// Select a crop of Paul Klee's "Dream City" and draw it using p5js as a 1024 x 768 pixel program.
+// First select an interesting/ambitious crop, then load it into a program such as Photoshop
+// to read the color and coordinate data. Your crop does not have to be 1024x768.
+// You should create the crop, then resize to 1024x768 and use that as your reference.
+// Use integer values for coordinates and only use the following functions for drawing:
+// line(), triangle(), quad(), rect(), ellipse(), and arc().
+// You must also use the stroke(), fill(), and background() functions in your sketch as well.
+// Your assignment will be graded on specified code criteria, as well as how accurately your
+// composition and colors match your selected crop.
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  createCanvas(1024,768);
+  background(0);
+  ellipse(50,50,100,100);
+  rect(35,0,35,65);
+
+}
